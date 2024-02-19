dd28fcc9a96b76c586a82317e6d581c02dc2563e
MichelaVivacqua <vivacquamichela@gmail.com>
2024-02-19 15:29:35 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
-import Sidebar from "./components/sidebar";
-
 function App() {
   return (
     <>
       <h1 className="bg-secondary">Team 7 too good</h1>
-      <Sidebar />
     </>
   );
 }
