badf3b64482ee334869c85e0cc069133052a2a55
Jovelasirot <jovelasirot@gmail.com>
2024-02-20 15:33:24 +0100

    body exp

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { Modal } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { newUserPost } from "../redux/actions/actions";
 
@@ -10,42 +9,91 @@ const MyForm = () => {
   const token = useSelector((state) => state.token.token) || undefined;
   const id = useSelector((state) => state.user.content._id) || undefined;
 
+  const [formData, setFormData] = useState({
+    role: "",
+    company: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    area: "",
+  });
+
+  const { role, company, startDate, endDate, description, area } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // dispatch(newUserPost(id, token, "testo"));
-    console.log("cliccato");
+    const experience = {
+      role,
+      company,
+      startDate,
+      endDate,
+      description,
+      area,
+    };
+
+    dispatch(newUserPost(id, token, experience));
+    console.log("Submitted");
   };
+
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group controlId="role">
         <Form.Label>Role</Form.Label>
-        <Form.Control type="text" placeholder="Enter role" />
+        <Form.Control
+          type="text"
+          placeholder="Enter role"
+          onChange={handleChange}
+        />
       </Form.Group>
 
       <Form.Group controlId="company">
         <Form.Label>Compagnia</Form.Label>
-        <Form.Control type="text" placeholder="Enter company" />
+        <Form.Control
+          type="text"
+          placeholder="Enter company"
+          onChange={handleChange}
+        />
       </Form.Group>
 
       <Form.Group controlId="startDate">
         <Form.Label>Inizio Attività</Form.Label>
-        <Form.Control type="text" placeholder="Enter start date" />
+        <Form.Control
+          type="text"
+          placeholder="Enter start date"
+          onChange={handleChange}
+        />
       </Form.Group>
 
       <Form.Group controlId="endDate">
         <Form.Label>Fine Attività</Form.Label>
-        <Form.Control type="text" placeholder="Enter end date" />
+        <Form.Control
+          type="text"
+          placeholder="Enter end date"
+          onChange={handleChange}
+        />
       </Form.Group>
 
       <Form.Group controlId="description">
         <Form.Label>Descrizione</Form.Label>
-        <Form.Control type="text" placeholder="Enter description" />
+        <Form.Control
+          type="text"
+          placeholder="Enter description"
+          onChange={handleChange}
+        />
       </Form.Group>
 
       <Form.Group controlId="area">
         <Form.Label>Area</Form.Label>
-        <Form.Control type="text" placeholder="Enter area" />
+        <Form.Control
+          type="text"
+          placeholder="Enter area"
+          onChange={handleChange}
+        />
       </Form.Group>
 
       <Button variant="primary" type="submit">
