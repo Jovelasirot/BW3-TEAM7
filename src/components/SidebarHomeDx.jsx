8d44201a04f179d98bd97c186370d62f66d3aa06
MichelaVivacqua <vivacquamichela@gmail.com>
2024-02-21 11:16:02 +0100

    completed sidebar dx

--- src/components/SidebarHomeDx.jsx
+++ src/components/SidebarHomeDx.jsx
@@ -3,47 +3,57 @@ import { ListGroup, Button } from "react-bootstrap";
 
 const SidebarHomeDx = () => {
   const items = [
-    "Elemento 1",
-    "Elemento 2",
-    "Elemento 3",
-    "Elemento 4",
-    "Elemento 5",
-    "Elemento 6",
-    "Elemento 7",
-    "Elemento 8",
-    "Elemento 9",
-    "Elemento 10",
+    { title: "• Come se la passano i pendolari", content: "1 giorno fa" },
+    { title: "• JS o TS? Pro e contro", content: "oggi" },
+    { title: "• Vite, CRA non ha più utenti", content: "3 giorni fa" },
+    { title: "• Epicoder richiesti in tutte le aziende", content: "14 ore fa" },
+    { title: "• Frontender specialist", content: "2 giorni fa" },
+    { title: "• Clonare LinkedIn", content: "2 ore fa" },
+    { title: "• Inverno Soleggiato", content: "7 giorni fa" },
+    { title: "• Non c'è neve in montagna", content: "19 ore fa" },
+    { title: "• Aperitivi al mare a Febbraio", content: "4 giorni fa" },
+    { title: "• TEAM 7 ♥", content: "oggi" },
   ];
 
   const [espandiLista, setEspandiLista] = useState(false);
 
   return (
-    <div className="col-lg-3 d-lg-none ">
-      <h5>LinkedIn Notizie</h5>
-      <ListGroup as="ul">
-        {items.slice(0, 5).map((item, index) => (
-          <ListGroup.Item key={index} as="li">
-            {item}
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-
-      {espandiLista && (
+    <section>
+      <div className="col-lg-3 d-none d-md-block ">
+        <div className="d-flex justify-content-between">
+          <h5 className="m-3">LinkedIn Notizie</h5>
+          <i className="bi bi-info-square-fill m-3"></i>
+        </div>
         <ListGroup as="ul">
-          {items.slice(5).map((item, index) => (
+          {items.slice(0, 5).map((item, index) => (
             <ListGroup.Item key={index} as="li">
-              {item}
+              <h6>{item.title}</h6>
+              <p>{item.content}</p>
             </ListGroup.Item>
           ))}
         </ListGroup>
-      )}
 
-      {items.length > 5 && (
-        <Button onClick={() => setEspandiLista(!espandiLista)} className="mt-3">
-          {espandiLista ? "Meno dettagli" : "Visualizza altro"}
-        </Button>
-      )}
-    </div>
+        {espandiLista && (
+          <ListGroup as="ul">
+            {items.slice(5).map((item, index) => (
+              <ListGroup.Item key={index} as="li">
+                <h6>{item.title}</h6>
+                <p>{item.content}</p>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+
+        {items.length > 5 && (
+          <Button
+            onClick={() => setEspandiLista(!espandiLista)}
+            className="mt-3"
+          >
+            {espandiLista ? "Meno dettagli" : "Vedi altro"}
+          </Button>
+        )}
+      </div>
+    </section>
   );
 };
 
