f0c06054b3dd141cb26cfcfab994566ec15b4258
Roberta Stira <151186486+robertastira@users.noreply.github.com>
2024-02-21 17:48:00 +0100

    sidebarcommit

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,19 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Container, Row } from "react-bootstrap";
+
+import { Container, Row, Col } from "react-bootstrap";
 import HomeDxFooter from "./HomeDxFooter";
+import JobSidebarSx from "./JobSidebarSx";
 
 function Job() {
   return (
-    <BrowserRouter>
-      <Container>
+      <Container fluid>
         <Row>
-          <Routes>
-            <Route />
-          </Routes>
-          <HomeDxFooter />
+          <JobSidebarSx/>
+          <Col className="col-lg-7 col-md-10 col-s-12"></Col>
+          <Col lg={3} className="d-none d-md-block"><HomeDxFooter />
+          </Col>
         </Row>
       </Container>
-    </BrowserRouter>
   );
 }
 
