925dcb11fba1ffa64d1e61b5251571c3e0e4e842
Roberta Stira <151186486+robertastira@users.noreply.github.com>
2024-02-21 12:37:21 +0100

    Commit2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import { Container, Row } from "react-bootstrap";
 import SidebarHomeDx from "./SidebarHomeDx";
+import SidebarHomeSx from "./SidebarHomeSx";
 
 const Home = () => {
   return (
     <Container fluid>
       <Row>
+        <SidebarHomeSx/>
         {/* sidebar sx col 2
         sez centrale col 7 */}
         <SidebarHomeDx />
