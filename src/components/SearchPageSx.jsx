42170067a634018dcc60b65d6e314e7ead048f04
Roberta Stira <151186486+robertastira@users.noreply.github.com>
2024-02-22 12:14:23 +0100

    SearchPageSx commit

--- src/components/SearchPageSx.jsx
+++ src/components/SearchPageSx.jsx
@@ -1,5 +1,10 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import dveloper from "../assets/dveloper.png"
+import epicodelogo from "../assets/epicodelogo.png"
+
+const studentiLavoratori = Math.floor(Math.random() * 99) + 1;
+const candidatiLavoratori = Math.floor(Math.random() * 99) + 1;
 
 function SearchPageSx() {
     return(
@@ -8,18 +13,41 @@ function SearchPageSx() {
         <Row>
           <Col>
           <section className="p-2 m-2 border border">
-                <div className="d-flex-column p-3">
-                  <p className="jobText fw-bold"><i className="bi bi-bookmark-fill jobsIcons me-2"></i>
-    Le mie offerte di lavoro</p>
-                  <p className="jobText fw-bold"><i className="bi bi-list-task jobsIcons me-1"></i>
-    Preferenze</p>
-    <p className="jobText fw-bold"><i className="bi bi-clipboard2-check jobsIcons me-1"></i>
-    Valutazione delle competenze</p>
-    <p className="jobText fw-bold"><i className="bi bi-youtube jobsIcons me-1"></i>
-    Indicazioni per chi cerca lavoro</p>
-    <p className="jobText fw-bold"><i className="bi bi-gear-fill jobsIcons me-1"></i>
-    Impostazioni candidatura</p>
-                 </div>
+                
+                <Row>
+                    
+                    <Col className="col-2">
+                    <img
+                    className="float-end"
+                    src={dveloper}
+                    alt="logo"
+                    style={{ width: '90%', height: 'auto' }}
+                    />
+                    </Col>
+
+                  <Col className="col-10">
+                  <div className="d-flex-column">
+                  <div className="bluetext1 fw-bold">Tipo di Lavoro</div>
+                  <div className="sidebarText1">Azienda</div>
+                  <div className="sidebarText2">Luogo</div>
+                  <div><span>
+                  <img
+                  src={epicodelogo}
+                  alt="logo"
+                  style={{ width: '4%', height: 'auto' }}/>
+                  </span>
+                  <span className="ms-1 sidebarText2">
+                  {studentiLavoratori} ex studenti lavorano qui</span>
+                  </div>
+
+                  <div className="mt-2"><span className="sidebarText2 fw-bold">1 giorno fa · </span>
+                  <span className="sidebarText2 greentext fw-bold">{candidatiLavoratori} candidati</span>
+                  </div>
+                  </div>
+                  <hr/>
+                  </Col>
+                  </Row>
+                
               </section>
     
           </Col>
