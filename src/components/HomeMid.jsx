92be36110d94589d590e8e28377320baee97afff
Jovelasirot <jovelasirot@gmail.com>
2024-02-23 16:17:14 +0100

    Commit refresh posts

--- src/components/HomeMid.jsx
+++ src/components/HomeMid.jsx
@@ -1,8 +1,12 @@
 import { Col, Container, Row } from "react-bootstrap";
 import HomeMidTop from "./HomeMidTop";
 import SinglePost from "./SinglePost";
+import { useDispatch, useSelector } from "react-redux";
+import { saveHomePost } from "../redux/actions/actions";
 
 const HomeMid = () => {
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token.token);
   return (
     <div className="col-lg-6 ">
       <Row className="flex-column">
@@ -14,10 +18,18 @@ const HomeMid = () => {
             <hr />
           </div>
           <div>
-            <span className="d-flex">
+            <span className="d-flex  ">
               Seleziona la visualizzazione del feed:
-              <p className="mb-0 fw-semibold ms-2 ">Più rilevanti per primi</p>
-              <i className="bi bi-caret-down-fill"></i>
+              <div className="addImg d-flex rounded-1 ">
+                <p
+                  className="mb-0 fw-semibold ms-2 "
+                  onClick={() => dispatch(saveHomePost(token))}
+                  style={{ cursor: "pointer" }}
+                >
+                  Più rilevanti per primi
+                </p>
+                <i className="bi bi-caret-down-fill"></i>
+              </div>
             </span>
           </div>
         </Col>
